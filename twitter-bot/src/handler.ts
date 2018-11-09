e8b9219d69889d2990977ef471808307a32d3319
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-11-09 10:42:49 -0700

    Fix bugs.

--- twitter-bot/src/handler.ts
+++ twitter-bot/src/handler.ts
@@ -1,5 +1,6 @@
 import {ScheduleEventHandler} from '../../libs/lambda-util/lambda-util';
-import * as Twitter from 'twitter';
+import {quotes} from './space-dandy';
+import Twitter = require('twitter');
 import moment = require('moment');
 import request = require('request-promise');
 
@@ -16,14 +17,14 @@ export interface Event {
 }
 
 const startDate = moment('2018-11-09T16:00:00.000Z');
-const quotes = require('./quotes');
 
-export const handler: ScheduleEventHandler<Event> = async (event, context) => {
+export const tweet: ScheduleEventHandler<Event> = async (event) => {
   if (event.type === 'Quote') {
     const now = moment();
     const hoursSinceStart = now.diff(startDate, 'hours');
-    const i =  Math.floor(hoursSinceStart / event.rate) % quotes.length;
+    const i = Math.floor(hoursSinceStart / event.rate) % quotes.length;
     const quote = quotes[i];
+    console.log(quote);
     const tweet = await client.post('statuses/update', {status: quote});
     console.log(tweet);
   }
@@ -32,9 +33,10 @@ export const handler: ScheduleEventHandler<Event> = async (event, context) => {
     const options = {
       method: 'GET',
       url: 'https://icanhazdadjoke.com/',
-      headers: {Accept: 'application/json'}
+      json: true
     };
     const {joke} = await request(options);
+    console.log(joke);
     const tweet = await client.post('statuses/update', {status: joke});
     console.log(tweet);
   }
